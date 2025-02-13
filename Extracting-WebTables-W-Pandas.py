95999ffb67dc903b385946cc0b85f75dbcf1ee28
seleniumforchitta <seleniumforchitta@gmail.com>
2025-02-13 10:56:39 +0530

    PR for extracting web tables using pandas

---
+++ Extracting-WebTables-W-Pandas.py
@@ -0,0 +1,15 @@
+# pip install pandas
+# pip install lxml 
+
+import pandas as pd
+
+nobel = pd.read_html('https://en.wikipedia.org/wiki/List_of_Nobel_laureates')
+print(len(nobel))
+print(nobel)
+
+# The pandas.read_html() function is used to extract tables from HTML content. Under the hood, 
+# it relies on external libraries to parse the HTML and convert it into a structured format (like a DataFrame). The two most common libraries for this are:
+# lxml: A fast and efficient library for processing XML and HTML.
+# html5lib: A pure Python library that parses HTML more leniently, similar to how web browsers do.
+# By default, pandas tries to use lxml because it is faster and more memory-efficient than html5lib. 
+# However, lxml is not included with Python or pandas by default, so you need to install it separately.
